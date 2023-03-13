1ac1f87deac8f46dfd49c733b0e69b74017e3da5
MAGIC <Vitaka145@gmail.com>
2023-03-13 22:28:40 +0200

    Не закончена комманда развода

--- cogs/marryage.py
+++ cogs/marryage.py
@@ -16,13 +16,14 @@ class marryage(commands.Cog):
     
 
 
-    # Предложенние
+    # Предложенние (Нужно проверить)
     @commands.command()
     async def marry(self, ctx, member):
         if member.id != ctx.author.id:
             # def check_author(m):
             #     return m.channel == ctx.channel and m.author == ctx.author
             member = message.mentions
+            guild = ctx.guild
             # Добавить проверку на пользователя
             # Если не указан, остановить выполнение задачи
             await ctx.send(f"""
@@ -51,7 +52,8 @@ class marryage(commands.Cog):
                     ctx.send("К сожалению у одного из вас не хватает баланса, для того чтоб сыграть свадьбу")
                 else:
                     try:
-                        db.query(f"INSERT INTO marryage (member_id, coupe_member_id, guild_id) VALUES ({member.id}, {message.author.id}, {author.id})")
+                        db.query(f"INSERT INTO marryage (member_id, coupe_member_id, guild_id) VALUES ({member.id}, {message.author.id}, {guild.id})")
+                        db.query(f"INSERT INTO marryage (member_id, coupe_member_id, guild_id) VALUES ({author.id}, {member.id}, {guild.id})")
                         db.query(f"UPDATE eco SET bank = {new_bal_a} WHERE member_id= {author.id} AND guild_id = {ctx.guild_id}")
                         db.query(f"UPDATE eco SET bank = {new_bal_m} WHERE member_id= {message.author.id} AND guild_id = {ctx.guild_id}")
                         return await ctx.send(f'''
@@ -75,7 +77,30 @@ class marryage(commands.Cog):
                 return await ctx.send(f'{member.mention} Вы ответили не коректно, повторите попытку в следуший раз')
         else:
             ctx.send("Детектор ЧСВ не выдержал перегрузки и сгорел")
-             
+        
+
+    @commands.command()
+    async def divorce(self, ctx, member):
+        if member.id != ctx.author.id:
+            author = ctx.author
+            db_coupe = db.query(f"SELECT coupe_member_id FROM marryage WHERE member_id={author.id}")
+            coupe_id = db_coupe[0]["coupe_member_id"]
+            if coupe_id != member.id:
+                ctx.send("Вы не можете подать на развод с человеком, который не имеет отношение к вам")
+                return
+            else:
+                def check_member(m):
+                    return m.channel == ctx.channel and m.member == ctx.member
+                try:
+                    message = await self.client.wait_for('message', check=check_member, timeout=300)
+                except asyncio.TimeoutError:
+                    return await ctx.send(embed=discord.Embed(description=f"{author.mention} Время ожидания ответа вышло! \n Похоже что {member.mention} проигнорировал/а ваше предложение о разводе.\n Попробуйте позже или обратитесь в суд!"))
+                if message.content == str("Да") or message.content == str("да"):
+                    db.query(f"UPDATE marryage SET coupe_member_id = null WHERE member_id= {author.id} AND guild_id = {ctx.guild_id}")
+                    db.query(f"UPDATE marryage SET coupe_member_id = null WHERE member_id= {member.id} AND guild_id = {ctx.guild_id}")
+                    await ctx.send("")
+                if message.content == str("Нет") or message.content == str("нет"):
+                    await ctx.send("")
 
 async def setup(client):
 	await client.add_cog(marryage(client))
